77a89ff97141c6ef37cbbdd95bec2e53e2f2de7c
Trong Khanh <lichessbotvietnam@gmail.com>
2025-03-30 13:58:17 +0700

    Update lichess_bot.py

--- lichess_bot.py
+++ lichess_bot.py
@@ -15,9 +15,6 @@ with open("key.txt", "r") as f:
     token = f.read().strip()
 
 logger = logging.getLogger(__name__)
-
-with open("lib/versioning.yml") as version_file:
-    versioning_info = yaml.safe_load(version_file)
  
 STOCKFISH_PATH = "./engines/stockfish-windows-x86-64-avx2.exe" # Adjust if needed
 
